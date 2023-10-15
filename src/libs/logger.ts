55615c7ca21990217cb99f21217a3ba0a5ea973e
aqshola <aqsholafid2@gmail.com>
2023-10-15 11:44:55 +0700

    back to default

--- src/libs/logger.ts
+++ src/libs/logger.ts
@@ -1,8 +1,6 @@
-import { Logger, pino } from "pino";
-import "pino-pretty";
+import pino from "pino";
 
 export const logger = pino({
-  level: process.env.NODE_ENV === "development" ? "debug" : "info",
   transport: {
     target: "pino-pretty",
     options: {
